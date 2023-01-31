4a8b85b1b0f5bdae81a1c1948109609d527ca915
phantomic12 <yoavtirosh54@gmail.com>
2023-01-31 12:27:33 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ cd "${INPUT_PKGDIR:-.}"
 # Assume that if .SRCINFO is missing then it is generated elsewhere.
 # AUR checks that .SRCINFO exists so a missing file can't go unnoticed.
 if [ -f .SRCINFO ] && ! sudo -u builder makepkg --printsrcinfo | diff - .SRCINFO; then
-	echo "::error file=$FILE,line=$LINENO::Mismatched .SRCINFO. Update with: makepkg --printsrcinfo > .SRCINFO"
+	sudo -u builder makepkg --printsrcinfo > .SRCINFO
 	exit 1
 fi
 
